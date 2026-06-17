3ae30da0a3071ee13ae3681861406efdf8888c9a
Shadin710 <shadin710@gmail.com>
2026-06-17 15:01:41 +0930

    Initial Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
